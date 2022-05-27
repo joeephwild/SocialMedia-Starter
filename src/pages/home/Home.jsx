af23ff8d82001f05b6a01dc078037796ff0ae835
joeephwild <josephomotade0@gmail.com>
2022-05-27 08:23:53 +0000

    initial commit

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Home.css"
+
+const Home = () => {
+  return (
+    <div className='Home'>
+        <div className='ProfileSide'></div>
+        <div className='PostSide'></div>
+        <div className='RightSide'></div>
+    </div>
+  )
+}
+
+export default Home
